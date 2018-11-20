fd8732587d205435c6cb876f755b7bbe17455bf6
Gabriel Redig <glredig@gmail.com>
2018-11-20 12:43:53 -0800

    more weighins actions

--- app/controllers/weighins_controller.rb
+++ app/controllers/weighins_controller.rb
@@ -4,4 +4,30 @@ class WeighinsController < ApplicationController
 
 		render json: @weighins
 	end
+
+	def create
+		@weighin = Weighin.new(weighin_params)
+
+		if @weighin.save
+			render json: @weighin, status: 201
+		else
+			render json: @weighin.errors.first.join(' ').capitalize, status: 422
+		end
+	end	
+
+	def show
+		@weighin = Weighin.find(params[:id])
+
+		if @weighin.present?
+			render json: @weighin
+		else
+			render json: { error: "Couldn't find weighin" }
+		end
+	end
+
+	protected
+
+	def weighin_params
+		params.require(:weighin).permit(:weight, :date)
+	end
 end
--- spec/requests/weighins_spec.rb
+++ spec/requests/weighins_spec.rb
@@ -16,4 +16,59 @@ RSpec.describe 'Weighins API', type: :request do
 			expect(response).to have_http_status(200)
 		end
 	end
+
+	describe 'GET /weighins/:id' do
+		before { get "/weighins/#{weighin_id}" }
+
+		context 'when the record exists' do
+			it 'returns the weighin' do
+				expect(json).not_to be_empty
+				expect(json['id']).to eq(weighin_id)
+			end
+
+			it 'returns status code 200' do
+				expect(response).to have_http_status(200)
+			end
+		end
+
+		context 'when the record does not exist' do
+			let(:weighin_id) { 300 }
+
+			it 'returns status code 404' do
+				expect(response).to have_http_status(404)
+			end
+
+			it 'returns a not found message' do
+				expect(response.body).to match(/Couldn't find Weighin/)
+			end
+		end
+	end
+
+	describe 'POST /weighins' do
+		let(:valid_attrs) { { weighin: { weight: 180.2, date: 1.day.ago }} }
+
+		context 'when the request is valid' do
+			before { post '/weighins', params: valid_attrs }
+
+			it 'creates a weighin' do
+				expect(json['weight']).to eq('180.2')
+			end
+
+			it 'returns status code 201' do
+				expect(response).to have_http_status(201)
+			end
+		end
+
+		context 'when the request is invalid' do
+			before { post '/weighins', params: { weighin: { weight: 'a' } }}
+
+			it 'returns status code 422' do
+				expect(response).to have_http_status(422)
+			end
+
+			it 'returns a failure message' do
+				expect(response.body).to match(/Date can't be blank/)
+			end
+		end
+	end
 end
